37c5ca3a7bb6b51bed5fced11fc0921b283fae09
LI_XE <hyangeun312@gmail.com>
2021-03-27 00:56:50 -0500

    updated assignment

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/GettingFamiliarWithRouting/src/main/java/com/xiangen/assignment/controllers/DojoController.java
@@ -0,0 +1,26 @@
+package com.xiangen.assignment.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DojoController {
+	
+	@RequestMapping("/dojo")
+	public String dojo() {
+		return "The dojo is awesome";
+	}
+	
+	@RequestMapping("/dojo/{location}")
+	public String dojoCenter(@PathVariable("location") String location) {
+		switch(location) {
+		case "burbank":
+			return "Burbank Dojo is located in Southern Califonia";
+		case "san-jose":
+			return "SJ dojo is the headquarters.";
+		default:
+			return String.format("The %s Dojo is great!", location);
+		}
+	}
+}
